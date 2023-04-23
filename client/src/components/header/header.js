f60cccd37db2a2261e2c12bac4dd108df808b9ad
slavik <slavik.zaliotnyi@gmail.com>
2023-04-23 04:24:42 +0300

    added logout btn in header

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -8,20 +8,37 @@ const Header = ({form}) => {
     }
 
     return (
-        <header className="header">
-            <nav className="header__nav">
-                <ul className="header__nav-list">
-                    <li className="header__nav-list-item"><Link to={'/'}>Головна</Link></li>
-                    {/* <li className="header__nav-list-item">Розмістити оголошення</li> */}
-                </ul>
+      <header className="header">
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-list-item">
+              <Link to={"/"}>Головна</Link>
+            </li>
+            {/* <li className="header__nav-list-item">Розмістити оголошення</li> */}
+          </ul>
 
-                <div className="header__button">
-                    <Link to={'/login'} onClick={onActiveForm} data-open='Enter' className="button">Вхід</Link>
-                    <Link to={'/singup'} onClick={onActiveForm} data-open='SingUp' className="button">Реєстрація</Link>
-                </div>
-            </nav>
-        </header>
-    )
+          <div className="header__button">
+            <Link
+              to={"/login"}
+              onClick={onActiveForm}
+              data-open="Enter"
+              className="button"
+            >
+              Вхід
+            </Link>
+            <Link
+              to={"/singup"}
+              onClick={onActiveForm}
+              data-open="SingUp"
+              className="button"
+            >
+              Реєстрація
+            </Link>
+            <button className="logout">Вийти з аккаунту</button>
+          </div>
+        </nav>
+      </header>
+    );
 }
 
 export default Header;
